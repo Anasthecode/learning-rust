d81a782586804ec0301008c30e2fc87edc98a7f1
Anasthecode <abdulaam@mcmaster.ca>
2026-01-11 18:27:24 -0500

    rust first steps

---
+++ hello.rs
@@ -0,0 +1,15 @@
+fn main() {
+  println!("Hello, World!");
+}
+
+// This is how you wirte a comment in rust
+/* Same syntax as C*/
+
+// fn main(), fn is short for function
+
+// Do rustc "fileName" on the terminal to compile the program
+// Executable is fileName.exe, runs using ./fileName
+
+// Cargo new helloProject creates a new directory with your code and a .gitignore
+// Cargo run to execute your file
+// Cargo practically auto compiles, unlike rustc
