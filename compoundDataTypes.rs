a29c75d80b2f6bd5ed84d1bb57c2f73816048304
Anasthecode <abdulaam@mcmaster.ca>
2026-01-11 19:28:21 -0500

    Arrays in rust,

---
+++ compoundDataTypes.rs
@@ -0,0 +1,18 @@
+// Compound data types
+// arrays, tuples, slices, strings and slice string
+
+// Arrays
+// like in C, and array needs have homogenoeous types and fixed size
+
+fn main() {
+  let numbers:[i32; 5] = [1, 2, 3, 4, 5];
+  // Array format: var [datatype; size]
+
+  // println!("Array: {}", numbers);
+  // In rust, there are debuggable and display format, will be taught later
+
+  println!("Array: {:?}", numbers);
+  // {:?} is one of many ways to express it
+
+}
+
